04f3699fcc0655ceea0d49e5f1973269ce0a1689
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-09-22 16:42:17 -0400

    positive unit test

--- tests/shared/piece/move-patterns/offset-pattern.spec.ts
+++ tests/shared/piece/move-patterns/offset-pattern.spec.ts
@@ -5,13 +5,17 @@ describe("Offset Pattern", () => {
 	it("Doesn't accept min iterations less than 1.", () => {
 		expect(() => {
 			new Offset_Pattern([0, 1], 0, Number.POSITIVE_INFINITY, true);
-		}).toThrow();
+		}).toThrowError();
 	});
 
 	it("Doesn't accept max iterations less than min iterations.", () => {
 		expect(() => {
 			new Offset_Pattern([0, 1], 3, 2, true);
-		}).toThrow();
+		}).toThrowError();
+	});
+
+	it("Accepts valid min and max iteration values.", () => {
+		new Offset_Pattern([0, 1], 1, 2, true);
 	});
 
 	// TODO: Add additional unit tests after Board class has been implemented
