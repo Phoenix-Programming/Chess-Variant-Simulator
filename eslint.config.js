1b5c4f754c26b02635c7f054d906ebfef3225e98
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-09-23 12:24:19 -0400

    updated eslint to stop yelling at me again
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,6 @@ export default defineConfig([
 ], {
   rules: {
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
+    "@typescript-eslint/no-unused-vars": "warn"
   }
 })
