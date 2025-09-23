df2c0464436f0833ba3e10f2e978121be3274687
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-09-23 09:18:08 -0400

    updated eslint to stop yelling about unused variables
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- eslint.config.js
+++ eslint.config.js
@@ -13,11 +13,16 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      reactRefresh.configs.vite
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-])
+      globals: globals.browser
+    }
+  }
+], {
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error"
+  }
+})
