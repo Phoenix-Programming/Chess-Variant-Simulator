fcc78c3384cf06fd6e0b4c904d73a072f319b2c9
Jordan Baumann <0501jordanb@gmail.com>
2025-09-25 20:25:15 -0400

    Added simple  implementation of getMovesFrom in Piece that uses a set.

--- src/shared/piece/piece.ts
+++ src/shared/piece/piece.ts
@@ -8,7 +8,18 @@ export class Piece {
         private patterns: Move_Pattern[]
     ){}
 
-    public getMovesFrom(Board: Board, piece: Piece_Ref): Move[] {
-        throw new Error("Unimplemented");
+    public getMovesFrom(board: Board, piece: Piece_Ref): Move[] {
+        // Uses a set to remove duplicates
+        let total_moves: Set<Move> = new Set<Move>();
+
+        for (let move_pattern of this.patterns) {
+            let new_moves = move_pattern.getMatches(board, piece);
+            // Adds each new move into total moves
+            // total_moves is a set this will remove duplicates for free
+            new_moves.forEach(total_moves.add, total_moves);
+        }
+
+        // Converts set back to array for easier use later
+        return Array.from(total_moves);
     }
 }
