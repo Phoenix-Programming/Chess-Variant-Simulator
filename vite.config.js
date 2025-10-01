994e68a6a9be0144ab4ecbbc74f9b0264f51320e
Daniel Freer <deletes_minimal.0w@icloud.com>
2025-10-01 00:57:06 -0400

    updated vite and tsconfig to move index.html to the src directory instead of the root
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+	root: "./src",
+	publicDir: "../public",
 	plugins: [react()],
 	css: {
 		devSourcemap: true
 	},
 	build: {
+		outDir: path.resolve(process.cwd(), 'dist'),
 		sourcemap: true,
 		emptyOutDir: false
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(process.cwd(), './'),
+			'@public': path.resolve(process.cwd(), './public'),
+			'@icons': path.resolve(process.cwd(), './public/icons')
+		}
 	}
 });
