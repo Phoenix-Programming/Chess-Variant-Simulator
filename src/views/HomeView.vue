3b4c9030b236e4dbf1ab04a9ca98a48a2db4d033
Daniel Freer <deletes_minimal.0w@icloud.com>
2026-02-22 15:26:59 -0500

    vue repo setup
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Chess Variant Simulator</h1>
+    <p>Welcome to the Chess Variant Simulator!</p>
   </main>
 </template>
