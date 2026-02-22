f12e350189442e5cde6785d44482b98d444d2096
Daniel Freer <deletes_minimal.0w@icloud.com>
2026-02-22 18:30:31 -0500

    fix example e2e test
    Signed-off-by: Daniel Freer <deletes_minimal.0w@icloud.com>

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,6 +1,6 @@
-describe("My First Test", () => {
+describe("Loads the app", () => {
   it("visits the app root url", () => {
     cy.visit("/");
-    cy.contains("h1", "You did it!");
+    cy.contains("h1", "Chess Variant Simulator");
   });
 });
